cc823079f9d64ef8b42603b7621fbd94c8ae4170
xflatcn <505574082@qq.com>
2019-04-13 14:33:45 +0800

    调整pom, 实现Application、controller、entity

---
+++ src/main/java/cn/xflat/cloud/client/controller/UserController.java
@@ -0,0 +1,19 @@
+package cn.xflat.cloud.client.controller;
+
+import cn.xflat.cloud.client.entity.User;
+import cn.xflat.cloud.client.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserFeignClient userFeignClient;
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        User user = this.userFeignClient.findById(id);
+        return user;
+    }
+}
