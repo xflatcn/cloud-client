c7d00b6e0b33f2db68ac9d474650918eeb0e18fd
xflatcn <505574082@qq.com>
2019-04-13 12:26:55 +0800

    commit cloud-provider、cloud-client module

---
+++ src/main/java/cn/xflat/cloud/client/CloudClientApplication.java
@@ -0,0 +1,13 @@
+package cn.xflat.cloud.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudClientApplication.class, args);
+    }
+
+}
